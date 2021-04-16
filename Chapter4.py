aedc67948a584a3af6236a27e41686e2a7994561
toddhumphrey <todd.h.humphrey@gmail.com>
2021-04-15 21:37:03 -0400

    Update for the end of chapter 4

--- Chapter4.py
+++ Chapter4.py
@@ -129,4 +129,84 @@ B = [[1, 2],     # B has 3 rows and 2 columns
      [5, 6]]
 
 #Please note that in normal math, rows and columns would be 1-indexed.  But since we are using python, we will zero-index our rows and columns in a matrix
+#Given the list-of-lists representation, a matrix's shape is the number of rows and number of columns
+#len(A) rows and len(A[0]) columns
 
+from typing import Tuple
+
+def shape(A: Matrix) -> Tuple[int, int]:
+    """Returns (# of rows of A, # of columns of A)"""
+    num_rows = len(A)
+    num_columns = len(A[0]) if A else 0 #number of elements in first row
+    return num_rows, num_columns
+
+assert shape([[1, 2, 3], [4, 5, 6]]) == (2, 3)
+
+#a matrix with n rows and k columns will be referred to as an n x k matrix
+#each row of an n x k matrix has a length of k
+#each column of an n x k matrix has a length of n
+
+def get_row(A: Matrix, i: int) -> Vector:
+    """Returns the i-th row of A (as a Vector)"""
+    return A[i]             # A[i] is already the ith row
+
+def get_column(A: Matrix, j: int) -> Vector:
+    """Returns the j-th column of A (as a Vector)"""
+    return [A_i[j]          # jth element of row A_i
+            for A_i in A]   # for each row A_i
+
+#We will end up wanting to create a matrix given its shape and a function for generating its elements using list comprehension
+
+from typing import Callable
+
+def make_matrix(num_rows: int,
+                num_cols: int,
+                entry_fn: Callable[[int, int], float]) -> Matrix:
+    """
+    Returns a num_rows x num_cols matrix
+    whose (i,j)-th entry is entry_fn(i, j)
+    """
+    return [[entry_fn(i, j)             # given i, create a list
+             for j in range(num_cols)]  #   [entry_fn(i, 0), ... ]
+            for i in range(num_rows)]   # create one list for each i
+
+
+def identity_matrix(n: int) -> Matrix:
+    """Returns the n x n identity matrix"""
+    return make_matrix(n, n, lambda i, j: 1 if i == j else 0)
+
+assert identity_matrix(5) == [[1, 0, 0, 0, 0],
+                              [0, 1, 0, 0, 0],
+                              [0, 0, 1, 0, 0],
+                              [0, 0, 0, 1, 0],
+                              [0, 0, 0, 0, 1]]
+
+data = [[70, 170, 40],
+        [65, 120, 26],
+        [77, 250, 19],
+        # ....
+       ]
+
+friendships = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
+               (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
+
+#            user 0  1  2  3  4  5  6  7  8  9
+#
+friend_matrix = [[0, 1, 1, 0, 0, 0, 0, 0, 0, 0],  # user 0
+                 [1, 0, 1, 1, 0, 0, 0, 0, 0, 0],  # user 1
+                 [1, 1, 0, 1, 0, 0, 0, 0, 0, 0],  # user 2
+                 [0, 1, 1, 0, 1, 0, 0, 0, 0, 0],  # user 3
+                 [0, 0, 0, 1, 0, 1, 0, 0, 0, 0],  # user 4
+                 [0, 0, 0, 0, 1, 0, 1, 1, 0, 0],  # user 5
+                 [0, 0, 0, 0, 0, 1, 0, 0, 1, 0],  # user 6
+                 [0, 0, 0, 0, 0, 1, 0, 0, 1, 0],  # user 7
+                 [0, 0, 0, 0, 0, 0, 1, 1, 0, 1],  # user 8
+                 [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]]  # user 9
+
+assert friend_matrix[0][2] == 1, "0 and 2 are friends"
+assert friend_matrix[0][8] == 0, "0 and 8 are not friends"
+
+# only need to look at one row
+friends_of_five = [i
+                   for i, is_friend in enumerate(friend_matrix[5])
+                   if is_friend]
