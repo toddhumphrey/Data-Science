dc1e8f5d3ddedf22eb55d7e5888934b510f4e04d
toddhumphrey <81595152+toddhumphrey@users.noreply.github.com>
2021-04-18 23:30:20 -0400

    Including beginning work on chapter 5

---
+++ Chapter5.py
@@ -0,0 +1,137 @@
+from collections import Counter
+from matplotlib import pyplot as plt
+
+num_friends = [100.0,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13,13,12,12,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+
+friend_counts = Counter(num_friends)
+xs = range(101) #largest value here is 100 since range goes from 1 to the number specified, but not inclusive
+ys = [friend_counts[x] for x in xs] #Height is just # of friends
+plt.bar(xs, ys)
+plt.axis([0, 101, 0, 25])
+plt.title("Histogram of Friend Counts")
+plt.xlabel("# of Friends")
+plt.ylabel("# of people")
+#plt.show()
+
+#Simple statistics to include
+
+#Size of dataset
+num_points = len(num_friends)               # 204
+
+
+assert num_points == 204
+
+#Largest and smallest values
+largest_value = max(num_friends)            # 100
+smallest_value = min(num_friends)           # 1
+
+
+assert largest_value == 100
+assert smallest_value == 1
+
+
+sorted_values = sorted(num_friends)
+smallest_value = sorted_values[0]           # 1
+second_smallest_value = sorted_values[1]    # 1
+second_largest_value = sorted_values[-2]    # 49
+
+
+assert smallest_value == 1
+assert second_smallest_value == 1
+assert second_largest_value == 49
+
+
+
+#Central Tendencies
+
+#Most commonly, we will want the mean of our dataset to see where our data is centered 
+
+from typing import List
+
+def mean(xs: List[float]) -> float:
+    return sum(xs) / len(xs)
+
+# The underscores indicate that these are "private" functions, as they're
+# intended to be called by our median function but not by other people
+# using our statistics library.
+def _median_odd(xs: List[float]) -> float:
+    """If len(xs) is odd, the median is the middle element"""
+    return sorted(xs)[len(xs) // 2]
+
+def _median_even(xs: List[float]) -> float:
+    """If len(xs) is even, it's the average of the middle two elements"""
+    sorted_xs = sorted(xs)
+    hi_midpoint = len(xs) // 2  # e.g. length 4 => hi_midpoint 2
+    return (sorted_xs[hi_midpoint - 1] + sorted_xs[hi_midpoint]) / 2
+
+def median(v: List[float]) -> float:
+    """Finds the 'middle-most' value of v"""
+    return _median_even(v) if len(v) % 2 == 0 else _median_odd(v)
+
+assert median([1, 10, 2, 9, 5]) == 5
+assert median([1, 9, 2, 10]) == (2 + 9) / 2
+
+
+assert median(num_friends) == 6
+
+def quantile(xs: List[float], p: float) -> float:
+    """Returns the pth-percentile value in x"""
+    p_index = int(p * len(xs))
+    return sorted(xs)[p_index]
+
+assert quantile(num_friends, 0.10) == 1
+assert quantile(num_friends, 0.25) == 3
+assert quantile(num_friends, 0.75) == 9
+assert quantile(num_friends, 0.90) == 13
+
+def mode(x: List[float]) -> List[float]:
+    """Returns a list, since there might be more than one mode"""
+    counts = Counter(x)
+    max_count = max(counts.values())
+    return [x_i for x_i, count in counts.items() 
+        if count == max_count]
+
+assert set(mode(num_friends)) == {1, 6}
+
+
+
+#Dispersion - describing the spread of our data
+
+def data_range(xs: List[float]) -> float:
+    return max(xs) - min(xs)
+
+assert data_range(num_friends) == 99
+
+def de_mean(xs: List[float]) -> List[float]:
+    """Translate xs by subtracting its mean (so the result has mean 0)"""
+    x_bar = mean(xs)
+    return [x - x_bar for x in xs]
+
+def sum_of_squares(devs: List[float]) -> float:
+    """Returns the sum of squares from a list of deviations"""
+    return sum(dev ** 2 for dev in devs)
+
+def variance(xs: List[float]) -> float:
+    """Almost the average squared deviation from the mean"""
+    assert len(xs) >= 2, "variance requires at least two elements"
+
+    n = len(xs)
+    deviations = de_mean(xs)
+    return sum_of_squares(deviations) / (n - 1)
+
+assert 81.54 < variance(num_friends) < 81.55
+
+import math
+
+def standard_deviation(xs: List[float]) -> float:
+    """Returns the standard deviation from a dataset"""
+    return math.sqrt(variance(xs))
+
+assert 9.02 < standard_deviation(num_friends) < 9.04
+
+def interquartile_range(xs: List[float]) -> float:
+    """Returns the difference between the 75%-ile and the 25%-ile"""
+    return quantile(xs, 0.75) - quantile(xs, 0.25)
+
+assert interquartile_range(num_friends) == 6
