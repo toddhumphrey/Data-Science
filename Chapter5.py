c80c405f3541876a9b0b458c755ab15e56e08299
toddhumphrey <81595152+toddhumphrey@users.noreply.github.com>
2021-04-19 22:55:54 -0400

    Update for another change to the end of chapter 5

--- Chapter5.py
+++ Chapter5.py
@@ -164,17 +164,17 @@ def correlation(xs: List[float], ys: List[float]) -> float:
 assert 0.24 < correlation(num_friends, daily_minutes) < 0.25
 assert 0.24 < correlation(num_friends, daily_hours) < 0.25
 
-    outlier = num_friends.index(100)    # index of outlier
+outlier = num_friends.index(100)    # index of outlier
 
-    num_friends_good = [x
-                        for i, x in enumerate(num_friends)
-                        if i != outlier]
+num_friends_good = [x
+                    for i, x in enumerate(num_friends)
+                    if i != outlier]
 
-    daily_minutes_good = [x
-                        for i, x in enumerate(daily_minutes)
-                        if i != outlier]
+daily_minutes_good = [x
+                    for i, x in enumerate(daily_minutes)
+                    if i != outlier]
 
-    daily_hours_good = [dm / 60 for dm in daily_minutes_good]
+daily_hours_good = [dm / 60 for dm in daily_minutes_good]
 
-    assert 0.57 < correlation(num_friends_good, daily_minutes_good) < 0.58
-    assert 0.57 < correlation(num_friends_good, daily_hours_good) < 0.58
+assert 0.57 < correlation(num_friends_good, daily_minutes_good) < 0.58
+assert 0.57 < correlation(num_friends_good, daily_hours_good) < 0.58
